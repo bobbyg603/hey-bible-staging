0955ae831a3214b48d79ea8dcd0f21652b5663ac
Bobby Galli <bobbyg603@gmail.com>
2026-02-15 19:15:21 -0500

    Fix: parse query params from deep link, add Browser.close()

--- chunk-FFDRGOYO.js
+++
@@ -1,2 +0,0 @@
-import{d as n}from"./chunk-GFJY36T2.js";import{a as r,b as t}from"./chunk-PGBQIJS2.js";function c(p){let e=n(p);return e?t(r({},e),{chapter:Number(e.chapter)}):null}export{c as a};
-//# sourceMappingURL=chunk-FFDRGOYO.js.map
---
+++ chunk-WLBF333T.js
@@ -0,0 +1,2 @@
+import{d as n}from"./chunk-PL6BJK5C.js";import{a as r,b as t}from"./chunk-PGBQIJS2.js";function c(p){let e=n(p);return e?t(r({},e),{chapter:Number(e.chapter)}):null}export{c as a};
+//# sourceMappingURL=chunk-WLBF333T.js.map
